8b9b0411c9ec4360d12da533585afc73ff51adff
Anton <antpavsor@gmail.com>
2020-10-09 23:40:06 +0300

    Настроен новый проект, написан тест на авторизацию

---
+++ fixture/application.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from fixture.session import SessionHelper
+
+
+class Application:
+
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+        self.wd.implicitly_wait(5)
+        self.session = SessionHelper(self)
+        self.base_url = base_url
+
+    def open_homepage(self):
+        wd = self.wd
+        wd.get(self.base_url)
+
+    def destroy(self):
+        self.wd.quit()
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
